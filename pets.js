2cb239c70ebe5e701dd924e1599df324eba895fe
YOUR FULL NAME <rachaelsvaldes@gmail.com>
2017-05-24 17:07:52 -0600

    all tests but one pasing

--- pets.js
+++ pets.js
@@ -1,41 +1,57 @@
 var fs = require('fs');
 var arr = process.argv;
-if (arr[2] === undefined) {
-  console.error('Usage: node pets.js [read | create | update | destroy]');
-  process.exit(1);
+var command = arr[2];
+
+function read(index) {
+  fs.readFile('pets.json', 'utf8', function(err, data) {
+      var content = JSON.parse(data);
+      if (index === 500) {
+        console.log(content);
+      }
+      if (content[index] !== undefined) {
+        console.log(content[index]);
+      }
+  });
 }
-else if (arr[2] === 'read') {
-  fs.readFile('pets.json', 'utf8', function (err, data) {
-    if (err) {
-  }
-  if (arr[3] === undefined) {
-    console.log(JSON.parse(data));
-  }
-  else {
-    var line = parseInt(arr[3]);
-    console.log(JSON.parse(data)[line]);
-  }
-});
+function create (age,kind,name) {
+  var pet = {};
+  pet.age = Number.parseInt(age);
+  pet.kind = kind;
+  no = name;
+  var petsJSON = JSON.stringify(pet);
+  console.log(pet);
+  fs.writeFile('pets.json', petsJSON, 'utf8', function(err) {
+    if (err) throw err;
+  })
+  read(2);
 }
-  else if (arr[2] === 'create') {
-    if (arr[3] === undefined) {
-      console.error('Usage: node pets.js create AGE KIND NAME');
-      process.exit(1);
+
+
+switch (command) {
+  case 'read':
+    var index = process.argv[3];
+    if (index === undefined) {
+      index = 500;
+      read(index);
     }
-    else if (arr[4] === undefined) {
-      console.error('Usage: node pets.js create AGE KIND NAME');
-      process.exit(1);
+    else if (index !== undefined) {
+      read(index);
     }
-    else if (arr[5] === undefined) {
+    break;
+  case 'create':
+    var age = process.argv[3];
+    var kind = process.argv[4];
+    var name = process.argv[5];
+    if (age === undefined || kind === undefined || name === undefined) {
       console.error('Usage: node pets.js create AGE KIND NAME');
       process.exit(1);
     }
     else {
-    //   var num = parseInt(arr[3]);
-    //   console.log(num);
-      var pet = {age: parseInt(arr[3]), kind: arr[4], name: arr[5]};
-      pets.push(pet);
-      console.log(pets);
-      process.exit(1);
+      create(age,kind,name);
     }
-  }
+    break;
+  default:
+    console.error('Usage: node pets.js [read | create | update | destroy]');
+    process.exit(1);
+     break;
+}
